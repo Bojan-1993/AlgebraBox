f7072fc76804cf472428ec638fce69bc7a1a94de
Bojan-1993 <bojan-1993@hotmail.com>
2017-01-24 12:38:22 +0100

    Nadam se da radi!

--- test.php
+++ test.php
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<php
-// Davor Medvedec
-=======
 <?php
 
-<<<<<<< HEAD
+
 #Bojan Oljača
-=======
-#Silvija Lušić
->>>>>>> 21a4cc00973ed3cdf73a23227a34650b0767f954
->>>>>>> 9819bb57b9d4bb1066aa92fb5ed07e57066f9499
